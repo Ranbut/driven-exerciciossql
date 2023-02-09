e787b58cfe0be8f62854075c2c4936d2a9d6ac55
Vitor Gabriel <59321138+Ranbut@users.noreply.github.com>
2023-02-08 21:10:39 -0300

    adicionado desafios

---
+++ 1-desafio.sql
@@ -0,0 +1,2 @@
+/*1. Busque todos os produtos com Televisão no nome. Para fazer isso, procure por sql like operator*/
+SELECT * FROM produtos WHERE nome LIKE '%Televisão%';
---
+++ 3-desafio.sql
@@ -0,0 +1,2 @@
+/*3. Busque todos os produtos com Celular no nome e preço menor que R$2000,00. Para isso, utilize o operador like do item anterior e procure por sql logica*/
+SELECT * FROM produtos WHERE nome LIKE '%Celular%' AND preco < 200000;
