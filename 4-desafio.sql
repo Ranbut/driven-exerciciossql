6c4a27288e9343aecbe090ed759e84484383cef1
Vitor Gabriel <59321138+Ranbut@users.noreply.github.com>
2023-02-08 21:46:07 -0300

    4 desafio

---
+++ 4-desafio.sql
@@ -0,0 +1,2 @@
+/*4. Busque todos os clientes que não fizeram uma compra. Para isso, procure por sql not in e sql subquery*/
+SELECT * FROM clientes WHERE id NOT IN (SELECT id_cliente FROM compras);
